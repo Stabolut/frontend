307f0223c179ae19fb3359ce1ffd5c6ffb6e8853
fahadazizahmed <fahadaziz307@yahoo.com>
2024-03-16 20:33:48 +0500

    feat: Add Not Found page for handling random URL hits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom"; // Importing routing components from react-router-dom
 import PurchaseWithEth from "./pages/purchase/purchaseWithEth"; // Importing component for purchasing with Ethereum
 import PurchaseWithBtc from "./pages/purchase/purchaseWithBtc"; // Importing component for purchasing with Bitcoin
+import NoFound from "./pages/NoPageFound/NoFound";
 
 function App() {
   // Render the application routes using BrowserRouter, Switch, and Route components
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/purchase-with-eth" component={PurchaseWithEth} />
         {/* Route for purchasing with Bitcoin */}
         <Route exact path="/purchase-with-btc" component={PurchaseWithBtc} />
+        <Route exact path="*" component={NoFound} />
       </Switch>
     </BrowserRouter>
   );
